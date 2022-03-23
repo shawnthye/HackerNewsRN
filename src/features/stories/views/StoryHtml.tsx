c03c68bc97187559bd952834b8e331c20a38843b
shawnthye <shawn.thye@gmail.com>
2022-03-24 02:09:44 +0800

    - handle back event for StoryHtml view

--- src/features/stories/views/StoryHtml.tsx
+++ src/features/stories/views/StoryHtml.tsx
@@ -3,6 +3,7 @@ import {StyleSheet} from 'react-native';
 import {RouteProp, useRoute} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import WebView from 'react-native-webview';
+import {useBackHandler} from '@react-native-community/hooks';
 import {useAppSelector} from '../../../core-store/hooks';
 import {selectStory} from '../data/stories-slice';
 
@@ -18,12 +19,33 @@ const StoryHtml = () => {
   const {
     params: {id},
   } = useRoute<RouteProps>();
-
   const story = useAppSelector(state => selectStory(state, id));
+  const html = `<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>${
+    story!.text
+  }</body></html>' }}`;
+
+  const webview = React.useRef<WebView>(null);
+
+  const [webviewCanGoBack, setCanGoBack] = React.useState<boolean>(false);
+
+  useBackHandler(() => {
+    if (webviewCanGoBack) {
+      webview.current?.goBack();
+      return true;
+    }
+
+    return false;
+  });
 
   return (
     <SafeAreaView style={styles.screen}>
-      <WebView source={{html: story!.text}} textZoom={200} />
+      <WebView
+        ref={webview}
+        source={{html: html}}
+        onNavigationStateChange={({canGoBack}) => {
+          setCanGoBack(canGoBack);
+        }}
+      />
     </SafeAreaView>
   );
 };
