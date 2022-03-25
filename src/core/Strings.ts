219ecc78acd65d98ac2d447f4fae30fcb308df77
shawnthye <shawn.thye@gmail.com>
2022-03-25 23:37:11 +0800

    - fixed typed error

--- src/core/Strings.ts
+++ src/core/Strings.ts
@@ -1,5 +1,5 @@
 export default class Strings {
-  static findHostname(url: string): string {
+  static findHostname(url: string | undefined): string | undefined {
     return url?.replace('https://', '').replace('http://', '').split('/')?.[0];
   }
 }
